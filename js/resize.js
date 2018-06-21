43282fabc169f338feba4d00e88e642250d52cc2
Steel Wagstaff <swagstaff@wisc.edu>
2018-06-21 11:53:41 -0500

    Update resize.js

--- js/resize.js
+++ js/resize.js
@@ -1,7 +1,11 @@
 window.hypothesisConfig = function () {
     return {
         onLayoutChange: function(layoutParams) {
-            jQuery('body').css({'padding-right':layoutParams.width});
+           // if the width of the browser minus the width of the hypothesis client is greater than 400 pixels, resize the html element.
+          if ((document.body.clientWidth - layoutParams.width) > 400) {
+            jQuery('html').css('width', '100%').css('width', '-=' + layoutParams.width + 'px');
+            // Still need to account for placement of PB nav buttons
         }
-    };
-};
+    }
+	}
+}
