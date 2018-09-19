4ef592d1cdd74a696b55ae2751f95ac95312cdd8
Steel Wagstaff <swagstaff@wisc.edu>
2018-09-19 12:47:21 -0500

    Update pagefit.js

--- js/pagefit.js
+++ js/pagefit.js
@@ -2,8 +2,10 @@ window.hypothesisConfig = function () {
     return {
         onLayoutChange: function(layoutParams) {
             var Hwidth = layoutParams.width();
+            // only triggers resizing actions if the main content has 400px of space after annotation pane is expanded
           if (body.width() - Hwidth > 400) {
-            jQuery('#page').css('margin-right', Hwidth + 'px');
+            jQuery('#page').css('margin-right', Hwidth + 'px'); // sets margin-right to equal the width of the annotation pane
+            jQuery('.nav-reading'.css('width', 'calc(100vw - ' + Hwidth + 'px'); // sets width of .nav-reading to viewport minus width of the annotation pane
             }
           }
         }
