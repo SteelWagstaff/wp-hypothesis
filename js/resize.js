13587de7ef6eb96ec3857b63a3e029f080b94fef
Steel Wagstaff <swagstaff@wisc.edu>
2018-09-19 09:44:20 -0500

    Update resize.js
    New method for resizing page dynamically

--- js/resize.js
+++ js/resize.js
@@ -3,8 +3,7 @@ window.hypothesisConfig = function () {
         onLayoutChange: function(layoutParams) {
            // if the width of the browser minus the width of the hypothesis client is greater than 400 pixels, resize the html element.
           if ((document.body.clientWidth - layoutParams.width) > 400) {
-            jQuery('html').css('width', '100%').css('width', '-=' + layoutParams.width + 'px');
-            // Still need to account for placement of PB nav buttons
+            jQuery('#page').css('margin-right', 'calc(100-' + layoutParams.width + 'px');
         }
     }
 	}
