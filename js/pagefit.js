0978920b024425d14c54da1e374b8c6c72cd0f32
Steel Wagstaff <swagstaff@wisc.edu>
2018-09-21 16:41:24 -0500

    update pagefit.js
    finalized working prototype. needs further refinement, but this should work.

--- js/pagefit.js
+++ js/pagefit.js
@@ -2,27 +2,12 @@ window.hypothesisConfig = function () {
     return {
         onLayoutChange: function (layoutParams) {
             var Hwidth = layoutParams.width;
-            if (body.width() - Hwidth > 400) {
-                jQuery('#page').css('margin-right', Hwidth + 'px');
+            var browserWidth = document.body.clientWidth;
+            // resize only if browser width minus annotation pane is greater than 400 pixels
+            if (browserWidth - Hwidth > 400) {
+                jQuery('#page').css('margin-right', Hwidth + 'px'); // sets right margin of page element to width of the annotation pane
+                jQuery('.nav-reading').css('width', 'calc(100vw - ' + Hwidth + 'px'); // sets width of .nav-reading to viewport minus width of the annotation pane
             }
         }
     };
 };
-/* Jon Udell's method for resizing pages
-    function adjust(e) {
-      var eWidth = parseInt(window.getComputedStyle(e)['width'].replace('px',''));
-      var bodyWidth = window.innerWidth;
-      var diff = bodyWidth - eWidth;
-      var marginLeft = window.getComputedStyle(e)['margin-left'];
-      var adjustment = 400;
-      if ( diff < adjustment ) {
-        var adjusted = (eWidth-(adjustment-diff)) + 'px';
-    	  e.style.width = adjusted;
-    	  e.style.marginLeft = marginLeft;
-      }
-    }
-
-    function adjustAll() {
-      document.querySelectorAll('*').forEach(e => adjust(e) );
-    }
-    adjustAll();
